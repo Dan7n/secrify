2c1ba6f26e195985a0bf0394e83533eb6f9c28f4
Danny Isaac <danny@Dannys-MacBook-Pro.local>
2022-08-04 22:50:37 +0200

    Updated metadata image

--- src/components/SecretHead.svelte
+++ src/components/SecretHead.svelte
@@ -9,12 +9,12 @@
 	<meta property="og:url" content="https://secrify.vercel.app/">
 	<meta property="og:title" content="Secret Message">
 	<meta property="og:description" content="Secrify is a tool to generate and share secret, encrypted messages using a simple one-time link.">
-	<meta property="og:image" content="/logo.png">
+	<meta property="og:image" content="https://i.imgur.com/Has1qCl.png">
 
 	<!-- Twitter -->
 	<meta property="twitter:card" content="summary_large_image">
 	<meta property="twitter:url" content="https://secrify.vercel.app/">
 	<meta property="twitter:title" content="Secret Message">
 	<meta property="twitter:description" content="Secrify is a tool to generate and share secret, encrypted messages using a simple one-time link.">
-	<meta property="twitter:image" content="/logo.png">
+	<meta property="twitter:image" content="https://i.imgur.com/Has1qCl.png">
 </svelte:head>
